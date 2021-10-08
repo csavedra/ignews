18bcafaa4820644453507562bd5b2ff64c685f97
csavedra <carlos.eduardosavedra71@gmail.com>
2021-10-08 18:08:24 -0300

    adding

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/client';
+import { route } from 'next/dist/server/router';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -9,12 +11,20 @@ interface SubscribeButton {
 
 export function SubscribeButton({ priceId }: SubscribeButton) {
     const [session] = useSession();
+    const router = useRouter();
 
     async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return;
         }
+
+        if (session.activeSubscription) {
+            router.push('/posts');
+            return;
+        }
+
+
         try {
             const response = await api.post('/subscribe')
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,37 +13,50 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+
   callbacks: {
     async session(session) {
-      const userActiveSubscription = await fauna.query(
-        q.Get(
-         q.Intersection([
-          q.Match(
-            q.Index('subscription_by_user_ref'),
-            q.Select(
-              "ref",
-              q.Get(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(session.user.email)
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
                 )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
               )
-            )
-          ),
-          q.Match(
-            q.Index('subscription_by_status'),
-            "active"
+            ])
           )
-        ])
         )
-      )
 
-      return session
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
     },
 
+
+
     async signIn(user, account, profile) {
+
       const { email } = user;
-      console.log(user, account)
 
       try {
         await fauna.query(
@@ -68,6 +81,8 @@ export default NextAuth({
             )
           )
         )
+
+
         return true
       } catch {
         return false
